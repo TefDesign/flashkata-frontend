29253c369488f61818b24a84892d1ee780f83c3a
Stef <stephane.savona@gmail.com>
2025-08-05 14:39:11 +0200

    init commit

---
+++ components/Switch.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Switch } from "react-native";
+import React from "react";
+
+const Switch = () => {
+  return <Switch />;
+};
+
+export default Switch;
+
+const styles = StyleSheet.create({});
