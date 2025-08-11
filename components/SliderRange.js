9cf38649f49a9166a4ef04b16aaf507d1afd2cb4
Stef <stephane.savona@gmail.com>
2025-08-11 14:23:13 +0200

    add challenge screen without code fetch

--- components/SliderRange.js
+++ components/SliderRange.js
@@ -1,7 +1,7 @@
-import { Slider } from '@react-native-assets/slider'
-import { View, Text, StyleSheet } from 'react-native';
-import { useState } from 'react';
-import theme from '../styles/themeLight';
+import { Slider } from "@react-native-assets/slider";
+import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import theme from "../styles/themeLight";
 
 const SliderRange = (props) => {
   const { mode } = props;
@@ -9,9 +9,9 @@ const SliderRange = (props) => {
   const [sliderValue, setSliderValue] = useState(5);
 
   const getMode = () => {
-    if (mode === 'time') {
+    if (mode === "time") {
       return `${sliderValue} min`;
-    } else if (mode === 'cards') {
+    } else if (mode === "cards") {
       return `${sliderValue} cartes`;
     }
     return `${sliderValue}`;
@@ -19,41 +19,40 @@ const SliderRange = (props) => {
 
   return (
     <View style={styles.container}>
-        <Text
-          style={{
-            position: 'absolute',
-            left: ((sliderValue - 5) / (15 - 5)) * 300 - 25,
-            top: -16,
-            fontSize: theme.fontSize.small,
-            fontWeight: 'bold',
-            color: theme.colors.text,
-            textAlign: 'center',
-          }}
-        >
-          {getMode()}
-        </Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={5}
-          maximumValue={15}
-          onValueChange={(value) => setSliderValue(value)}
-          step={1}
-          value={sliderValue}
-          minimumTrackTintColor={theme.colors.success}
-          maximumTrackTintColor={theme.colors.border}
-          thumbTintColor={theme.colors.text}
-          trackHeight={10}
-          thumbSize={26}
-        />
+      <Text
+        style={{
+          position: "absolute",
+          left: ((sliderValue - 5) / (15 - 5)) * 300 - 25,
+          top: -16,
+          fontSize: theme.fontSize.small,
+          fontWeight: "bold",
+          color: theme.colors.text,
+          textAlign: "center",
+        }}
+      >
+        {getMode()}
+      </Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={5}
+        maximumValue={15}
+        onValueChange={(value) => setSliderValue(value)}
+        step={1}
+        value={sliderValue}
+        minimumTrackTintColor={theme.colors.success}
+        maximumTrackTintColor={theme.colors.border}
+        thumbTintColor={theme.colors.text}
+        trackHeight={10}
+        thumbSize={26}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
   slider: {
     width: 300,
