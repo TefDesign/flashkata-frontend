f607cc9058d48378ff7da52e42bc41a1fb00f753
Stef <stephane.savona@gmail.com>
2025-08-13 09:17:48 +0200

    datas temp

---
+++ datas/datas.js
@@ -0,0 +1,62 @@
+export const cards = [
+  {
+    id: 1,
+    name: "a",
+    image: "01-hiragana-a",
+    sound: "a",
+  },
+  {
+    id: 2,
+    name: "i",
+    image: "02-hiragana-i",
+    sound: "i",
+  },
+  {
+    id: 3,
+    name: "u",
+    image: "03-hiragana-u",
+    sound: "u",
+  },
+  {
+    id: 4,
+    name: "e",
+    image: "04-hiragana-e",
+    sound: "e",
+  },
+  {
+    id: 5,
+    name: "o",
+    image: "05-hiragana-o",
+    sound: "o",
+  },
+  {
+    id: 6,
+    name: "ka",
+    image: "06-hiragana-ka",
+    sound: "ka",
+  },
+  {
+    id: 7,
+    name: "ki",
+    image: "07-hiragana-ki",
+    sound: "ki",
+  },
+  {
+    id: 8,
+    name: "ku",
+    image: "08-hiragana-ku",
+    sound: "ku",
+  },
+  {
+    id: 9,
+    name: "ke",
+    image: "09-hiragana-ke",
+    sound: "ke",
+  },
+  {
+    id: 10,
+    name: "ko",
+    image: "10-hiragana-ko",
+    sound: "ko",
+  },
+];
