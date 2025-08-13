e34e4d7ca1d1078310dc4f6343475a8a070bca85
canadas hugo <canadashcb@gmail.com>
2025-08-13 11:31:49 +0200

    darkMode ok

--- screens/KanaScreen.js
+++ screens/KanaScreen.js
@@ -2,21 +2,107 @@ import {
   StyleSheet,
   Text,
   View,
-  KeyboardAvoidingView,
-  Platform,
   Pressable,
-  FlatList
+  Dimensions
 } from "react-native";
 import theme from "../styles/themeLight";
 import LogoIcon from "../assets/icons/logo.svg";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { API_URL } from "@env";
-import { svgMap, getSvgRequire } from "../utils/svgMap";
+import { getSvgRequire } from "../utils/svgMap";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import HeaderSecondary from "../components/HeaderSecondary";
+import useThemedStyles from "../hooks/useThemedStyles";
+import { HIRAGANA_SOUNDS } from "../utils/soundsMap";
 
 export default function KanaScreen({ navigation, route }) {
+
+  const [theme, styles] = useThemedStyles((theme) =>
+    StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        alignItems: "center",
+        paddingHorizontal: theme.spacing.large,
+        height: Dimensions.get("window").height,
+      },
+      logo: {
+        marginBottom: 20,
+      },
+      title: {
+        fontFamily: theme.fonts.staatliches,
+        fontSize: theme.fontSize.menu,
+        margin: theme.spacing.medium,
+        marginBottom: theme.spacing.large,
+        color: theme.colors.text,
+      },
+      textLarge: {
+        fontFamily: theme.fonts.outfitRegular,
+        fontSize: theme.fontSize.textLarge,
+        alignSelf: "flex-end",
+        color: theme.colors.text,
+        margin: 10,
+        paddingRight: 50
+      },
+      scrollContainer: {
+        flex: 1,
+        width: "100%",
+        maxHeight: "55%",
+      },
+      sectionText: {
+        width: "100%",
+        gap: theme.spacing.small,
+        marginBottom: theme.spacing.large,
+      },
+      progressionContainer: {
+        alignItems: "center",
+        textAlign: "center",
+        justifyContent: "center",
+        marginBottom: theme.spacing.medium,
+        marginTop: theme.spacing.medium,
+        height: 60,
+        marginTop: 20
+      },
+      kanaCard: {
+        width: '60%',
+        aspectRatio: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 8,
+        margin: 1,
+        borderWidth: 3,
+        borderRadius: 20,
+        marginTop: '20%',
+        marginBottom: "10%",
+        position: 'relative',
+      },
+      fallbackContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      kanaText: {
+        color: theme.colors.text,
+        fontSize: 20,
+        fontFamily: theme.fonts?.outfit || "System",
+        marginTop: 5,
+        textAlign: "center",
+      },
+      starContainer: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        zIndex: 1,
+      },
+      cardContainer: {
+        flex: 1,
+        alignItems: "center",
+        marginBottom: 20,
+      },
+    })
+  );
+
 
   const { kana, type, index, getProgress } = route.params;
 
@@ -60,33 +146,34 @@ export default function KanaScreen({ navigation, route }) {
 
   const makeFavorite = async () => {
     try {
-    console.log('aaa')
-    const resp = await fetch(`${API_URL}/progress/kataProgress/modify`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        token: user.token,
-        userId: user.id,
-        katakana: type === "katakana" ? kana.katakanaId : kana.hiraganaId,
-        isFavorite: !favorite
+      console.log('aaa')
+      const resp = await fetch(`${API_URL}/progress/kataProgress/modify`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          token: user.token,
+          userId: user.id,
+          katakana: type === "katakana" ? kana.katakanaId : kana.hiraganaId,
+          isFavorite: !favorite
+        })
       })
-    })
-    const data = await resp.json()
-    console.log('recu : ', data)
-    setFavorite(data.data.isFavorite)
-    console.log(data.data.isFavorite)
-    getProgress()
-  } catch (err) {
-    console.log(err)
-  }
+      const data = await resp.json()
+      console.log('recu : ', data)
+      setFavorite(data.data.isFavorite)
+      console.log(data.data.isFavorite)
+      getProgress()
+    } catch (err) {
+      console.log(err)
+    }
 
   }
 
 
   return (
     <View style={styles.container}>
+      <HeaderSecondary />
       <View style={styles.logo}>
         <LogoIcon width={200} height={200} />
       </View>
@@ -95,7 +182,7 @@ export default function KanaScreen({ navigation, route }) {
         <View style={[styles.kanaCard, {
           borderColor: borderColor(priority),
           opacity: opacity(nbViews),
-          backgroundColor: favorite ? "#ffffbcff" : "#f9f9f9"
+          backgroundColor: favorite ? "#ffff0087" : "#f9f9f96c"
         }]}>
           {/* Étoile de favori en position absolue */}
           {
@@ -120,70 +207,11 @@ export default function KanaScreen({ navigation, route }) {
           <Text style={styles.kanaText}>{kanaName}</Text>
         </View>
         <View style={styles.subContainer}>
-          <Text>Vous avez vue la carte : <Text style={{ color: "blue" }}>{nbViews}</Text> fois</Text>
-          <Text>Vous avez répondu correctement : <Text style={{ color: "green" }}>{nbCorrect}</Text> fois</Text>
-          <Text>Vous avez vue la carte : <Text style={{ color: "red" }}>{nbWrong}</Text> fois</Text>
+          <Text style={styles.textLarge}>Vue : <Text style={{ color: "blue" }}>{nbViews}</Text> fois</Text>
+          <Text style={styles.textLarge}>Correcte : <Text style={{ color: "green" }}>{nbCorrect}</Text> fois</Text>
+          <Text style={styles.textLarge}>Erreur : <Text style={{ color: "red" }}>{nbWrong}</Text> fois</Text>
         </View>
       </View>
     </View>
   );
 };
-
-
-const styles = StyleSheet.create({
-  logo: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    paddingTop: 20,
-  },
-  cardContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  row: {
-    justifyContent: 'space-around',
-  },
-  kanaCard: {
-    width: '60%',
-    aspectRatio: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-    margin: 1,
-    borderWidth: 3,
-    borderRadius: 20,
-    position: 'relative', // Pour permettre le positionnement absolu de l'étoile
-  },
-  starContainer: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    zIndex: 1,
-  },
-  fallbackContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  kanaText: {
-    fontSize: 40,
-    fontFamily: theme.fonts?.outfit || "System",
-    marginTop: 5,
-    textAlign: "center",
-  },
-  subContainer: {
-    marginTop: 20,
-    width: '80%',
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 20,
-    paddingBottom: 20,
-    margin: 1,
-    borderWidth: 1,
-    borderRadius: 10,
-  }
-});
