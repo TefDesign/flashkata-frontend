a70ad884637a6e165f0138f3570e72c4eab1f2c1
canadas hugo <canadashcb@gmail.com>
2025-08-14 13:29:04 +0200

    mappage

--- utils/svgMap.js
+++ utils/svgMap.js
@@ -22,6 +22,32 @@ import HiraganaCHI from "../assets/icons/kata/hiragana/17-hiragana-chi.svg";
 import HiraganaTSU from "../assets/icons/kata/hiragana/18-hiragana-tsu.svg";
 import HiraganaTE from "../assets/icons/kata/hiragana/19-hiragana-te.svg";
 import HiraganaTO from "../assets/icons/kata/hiragana/20-hiragana-to.svg";
+import HiraganaNA from "../assets/icons/kata/hiragana/21-hiragana-na.svg";
+import HiraganaNI from "../assets/icons/kata/hiragana/22-hiragana-ni.svg";
+import HiraganaNU from "../assets/icons/kata/hiragana/23-hiragana-nu.svg";
+import HiraganaNE from "../assets/icons/kata/hiragana/24-hiragana-ne.svg";
+import HiraganaNO from "../assets/icons/kata/hiragana/25-hiragana-no.svg";
+import HiraganaHA from "../assets/icons/kata/hiragana/26-hiragana-ha.svg";
+import HiraganaHI from "../assets/icons/kata/hiragana/27-hiragana-hi.svg";
+import HiraganaFU from "../assets/icons/kata/hiragana/28-hiragana-fu.svg";
+import HiraganaHE from "../assets/icons/kata/hiragana/29-hiragana-he.svg";
+import HiraganaHO from "../assets/icons/kata/hiragana/30-hiragana-ho.svg";
+import HiraganaMA from "../assets/icons/kata/hiragana/31-hiragana-ma.svg";
+import HiraganaMI from "../assets/icons/kata/hiragana/32-hiragana-mi.svg";
+import HiraganaMU from "../assets/icons/kata/hiragana/33-hiragana-mu.svg";
+import HiraganaME from "../assets/icons/kata/hiragana/34-hiragana-me.svg";
+import HiraganaMO from "../assets/icons/kata/hiragana/35-hiragana-mo.svg";
+import HiraganaYA from "../assets/icons/kata/hiragana/36-hiragana-ya.svg";
+import HiraganaYU from "../assets/icons/kata/hiragana/38-hiragana-yu.svg";
+import HiraganaYO from "../assets/icons/kata/hiragana/40-hiragana-yo.svg";
+import HiraganaRA from "../assets/icons/kata/hiragana/41-hiragana-ra.svg";
+import HiraganaRI from "../assets/icons/kata/hiragana/42-hiragana-ri.svg";
+import HiraganaRU from "../assets/icons/kata/hiragana/43-hiragana-ru.svg";
+import HiraganaRE from "../assets/icons/kata/hiragana/44-hiragana-re.svg";
+import HiraganaRO from "../assets/icons/kata/hiragana/45-hiragana-ro.svg";
+import HiraganaWA from "../assets/icons/kata/hiragana/46-hiragana-wa.svg";
+import HiraganaWO from "../assets/icons/kata/hiragana/50-hiragana-wo.svg";
+import HiraganaN from "../assets/icons/kata/hiragana/51-hiragana-n.svg";
 
 //Katakana
 import KatakanaA from "../assets/icons/kata/katakana/01-katakana-a.svg";
@@ -44,6 +70,32 @@ import KatakanaCHI from "../assets/icons/kata/katakana/17-katakana-chi.svg";
 import KatakanaTSU from "../assets/icons/kata/katakana/18-katakana-tsu.svg";
 import KatakanaTE from "../assets/icons/kata/katakana/19-katakana-te.svg";
 import KatakanaTO from "../assets/icons/kata/katakana/20-katakana-to.svg";
+import KatakanaNA from "../assets/icons/kata/katakana/21-katakana-na.svg";
+import KatakanaNI from "../assets/icons/kata/katakana/22-katakana-ni.svg";
+import KatakanaNU from "../assets/icons/kata/katakana/23-katakana-nu.svg";
+import KatakanaNE from "../assets/icons/kata/katakana/24-katakana-ne.svg";
+import KatakanaNO from "../assets/icons/kata/katakana/25-katakana-no.svg";
+import KatakanaHA from "../assets/icons/kata/katakana/26-katakana-ha.svg";
+import KatakanaHI from "../assets/icons/kata/katakana/27-katakana-hi.svg";
+import KatakanaFU from "../assets/icons/kata/katakana/28-katakana-fu.svg";
+import KatakanaHE from "../assets/icons/kata/katakana/29-katakana-he.svg";
+import KatakanaHO from "../assets/icons/kata/katakana/30-katakana-ho.svg";
+import KatakanaMA from "../assets/icons/kata/katakana/31-katakana-ma.svg";
+import KatakanaMI from "../assets/icons/kata/katakana/32-katakana-mi.svg";
+import KatakanaMU from "../assets/icons/kata/katakana/33-katakana-mu.svg";
+import KatakanaME from "../assets/icons/kata/katakana/34-katakana-me.svg";
+import KatakanaMO from "../assets/icons/kata/katakana/35-katakana-mo.svg";
+import KatakanaYA from "../assets/icons/kata/katakana/36-katakana-ya.svg";
+import KatakanaYU from "../assets/icons/kata/katakana/38-katakana-yu.svg";
+import KatakanaYO from "../assets/icons/kata/katakana/40-katakana-yo.svg";
+import KatakanaRA from "../assets/icons/kata/katakana/41-katakana-ra.svg";
+import KatakanaRI from "../assets/icons/kata/katakana/42-katakana-ri.svg";
+import KatakanaRU from "../assets/icons/kata/katakana/43-katakana-ru.svg";
+import KatakanaRE from "../assets/icons/kata/katakana/44-katakana-re.svg";
+import KatakanaRO from "../assets/icons/kata/katakana/45-katakana-ro.svg";
+import KatakanaWA from "../assets/icons/kata/katakana/46-katakana-wa.svg";
+import KatakanaWO from "../assets/icons/kata/katakana/50-katakana-wo.svg";
+import KatakanaN from "../assets/icons/kata/katakana/51-katakana-n.svg";
 
 export const svgMap = {
   "01-hiragana-a": HiraganaA,
@@ -66,6 +118,32 @@ export const svgMap = {
   "18-hiragana-tsu": HiraganaTSU,
   "19-hiragana-te": HiraganaTE,
   "20-hiragana-to": HiraganaTO,
+  "21-hiragana-na": HiraganaNA,
+  "22-hiragana-ni": HiraganaNI,
+  "23-hiragana-nu": HiraganaNU,
+  "24-hiragana-ne": HiraganaNE,
+  "25-hiragana-no": HiraganaNO,
+  "26-hiragana-ha": HiraganaHA,
+  "27-hiragana-hi": HiraganaHI,
+  "28-hiragana-fu": HiraganaFU,
+  "29-hiragana-he": HiraganaHE,
+  "30-hiragana-ho": HiraganaHO,
+  "31-hiragana-ma": HiraganaMA,
+  "32-hiragana-mi": HiraganaMI,
+  "33-hiragana-mu": HiraganaMU,
+  "34-hiragana-me": HiraganaME,
+  "35-hiragana-mo": HiraganaMO,
+  "36-hiragana-ya": HiraganaYA,
+  "38-hiragana-yu": HiraganaYU,
+  "40-hiragana-yo": HiraganaYO,
+  "41-hiragana-ra": HiraganaRA,
+  "42-hiragana-ri": HiraganaRI,
+  "43-hiragana-ru": HiraganaRU,
+  "44-hiragana-re": HiraganaRE,
+  "45-hiragana-ro": HiraganaRO,
+  "46-hiragana-wa": HiraganaWA,
+  "50-hiragana-wo": HiraganaWO,
+  "51-hiragana-n": HiraganaN,
   "01-katakana-a": KatakanaA,
   "02-katakana-i": KatakanaI,
   "03-katakana-u": KatakanaU,
@@ -86,6 +164,32 @@ export const svgMap = {
   "18-katakana-tsu": KatakanaTSU,
   "19-katakana-te": KatakanaTE,
   "20-katakana-to": KatakanaTO,
+  "21-katakana-na": KatakanaNA,
+  "22-katakana-ni": KatakanaNI,
+  "23-katakana-nu": KatakanaNU,
+  "24-katakana-ne": KatakanaNE,
+  "25-katakana-no": KatakanaNO,
+  "26-katakana-ha": KatakanaHA,
+  "27-katakana-hi": KatakanaHI,
+  "28-katakana-fu": KatakanaFU,
+  "29-katakana-he": KatakanaHE,
+  "30-katakana-ho": KatakanaHO,
+  "31-katakana-ma": KatakanaMA,
+  "32-katakana-mi": KatakanaMI,
+  "33-katakana-mu": KatakanaMU,
+  "34-katakana-me": KatakanaME,
+  "35-katakana-mo": KatakanaMO,
+  "36-katakana-ya": KatakanaYA,
+  "38-katakana-yu": KatakanaYU,
+  "40-katakana-yo": KatakanaYO,
+  "41-katakana-ra": KatakanaRA,
+  "42-katakana-ri": KatakanaRI,
+  "43-katakana-ru": KatakanaRU,
+  "44-katakana-re": KatakanaRE,
+  "45-katakana-ro": KatakanaRO,
+  "46-katakana-wa": KatakanaWA,
+  "50-katakana-wo": KatakanaWO,
+  "51-katakana-n": KatakanaN,
 };
 
 export function getSvgRequire(filename) {
